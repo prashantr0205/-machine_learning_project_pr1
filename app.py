8d74e70b720caa1f4d10d3ac8549e41126f29581
prashant <prashant.r0205@gmail.com>
2022-07-20 12:55:41 +0530

    exception file succesfully created

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
-
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
@@ -7,8 +9,13 @@ app=Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 
 def index():
-    return"starting trial"
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing= HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+    return"CICD pipeline has been created"
 
 if __name__=="__main__":
     app.run(debug=True)
-
