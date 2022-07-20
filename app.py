66c3410e1e5706ec52064dee87b9cc3e1ebc35f3
prashant <prashant.r0205@gmail.com>
2022-07-20 10:02:40 +0530

    flask app created

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app=Flask(__name__)
+
+
+@app.route('/', methods=['GET','POST'])
+
+def index():
+    return"starting trial"
+
+if __name__=="__main__":
+    app.run(debug=True)
+
